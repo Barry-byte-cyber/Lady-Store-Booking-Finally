130fd258e5018284dad77613195eaf6cc8aa35a1
Barry-byte-cyber <metisman@telus.net>
2025-07-12 17:31:10 -0600

    Add BookingForm and AdminView components to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
+import BookingForm from "./components/BookingForm";
 import AdminView from "./Components/AdminView";
-
 function App() {
-  return (
-    <div>
-      <AdminView />
-    </div>
-  );
-}
+ return (
+  <div>
+    <h1>Lady Pant Store Booking</h1>
+    <BookingForm />
+    <hr />
+    <AdminView />
+  </div>
+);
 
 export default App;
