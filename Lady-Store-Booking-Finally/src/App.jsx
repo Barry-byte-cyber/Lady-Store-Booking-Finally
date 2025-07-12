597175034b5630684687a2f3a7d4669f8ec4de19
Barry-byte-cyber <metisman@telus.net>
2025-07-11 19:29:54 -0600

    Rebuilt project with fixed paths and working Vite config

--- Lady-Store-Booking-Finally/src/App.jsx
+++
@@ -1,5 +0,0 @@
-function App() {
-  return <h1>Lady Pant Store Booking is LIVE!</h1>;
-}
-
-export default App;
---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div>
+      <h1>Lady Pant Store Booking is LIVE!</h1>
+    </div>
+  );
+}
+
+export default App;
