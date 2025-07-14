d2624a9956d2da3f1091fa4aab1d997159cd3354
Barry-byte-cyber <metisman@telus.net>
2025-07-14 13:37:04 -0600

    Fix: Cleaned up App.jsx export and syntax

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,14 @@ import React from "react";
 import BookingForm from "./components/BookingForm";
 import AdminView from "./Components/AdminView";
 function App() {
- return (
-  <div>
-    <h1>Lady Pant Store Booking</h1>
-    <BookingForm />
-    <hr />
-    <AdminView />
-  </div>
-);
+  return (
+    <div>
+      <h1>Lady Pant Store Booking</h1>
+      <BookingForm />
+      <hr />
+      <AdminView />
+    </div>
+  );
+}
 
 export default App;
