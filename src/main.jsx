0afd52016d6dc9887bdb8fe6110bb660d9db9dab
Barry-byte-cyber <metisman@telus.net>
2025-07-18 11:41:40 -0600

    Fix case issue: Rename app.jsx to App.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import app from './app';
+import App from './App';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <app />
+    <App />
   </React.StrictMode>
 );
